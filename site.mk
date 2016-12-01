6a0952a6d0ec81c4d7e1f9e3670c47bb6667692a
Tom <tsiener@gmx.de>
2016-12-02 00:41:45 +0100

    site.mk - v019f vereinf.SSID-Changer
    v09f mit vereinfachtem SSID-Changer

--- site.mk
+++ site.mk
@@ -147,7 +147,7 @@ endif
 #                       opkg compare-versions "$1" '>>' "$2"
 #               to decide if a version is newer or not.
 
-DEFAULT_GLUON_RELEASE := ffwss-v019e
+DEFAULT_GLUON_RELEASE := ffwss-v019f
 
 #       GLUON_RELEASE
 #               call make with custom GLUON_RELEASE flag, to use your own release version scheme.
