b0c870f88ca0572beba063c61a7da7d02c8ccdc7
Tom <tsiener@gmx.de>
2017-03-25 02:04:45 +0100

    update site.mk - v021bs

--- site.mk
+++ site.mk
@@ -152,7 +152,7 @@ endif
 #                       opkg compare-versions "$1" '>>' "$2"
 #               to decide if a version is newer or not.
 
-DEFAULT_GLUON_RELEASE := ffwss-v021a
+DEFAULT_GLUON_RELEASE := ffwss-v021bs
 
 #       GLUON_RELEASE
 #               call make with custom GLUON_RELEASE flag, to use your own release version scheme.
