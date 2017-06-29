0a37b96c42e6b9b0f6fb59b03fc9ded7fc478ca0
Tom <tsiener@gmx.de>
2017-06-30 00:51:44 +0200

    site,mk  gluon-web-network anstelle portconfig

--- site.mk
+++ site.mk
@@ -16,7 +16,7 @@ GLUON_SITE_PACKAGES := \
         gluon-ebtables-filter-ra-dhcp \
         gluon-web-admin \
         gluon-web-autoupdater \
-        gluon-web-portconfig \
+        gluon-web-network \
         gluon-web-private-wifi \
         gluon-web-wifi-config \
 		gluon-mesh-batman-adv-15 \
