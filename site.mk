a4c3d8aab9da627ef83c046ee05c59293ffdf1a2
Tom <tsiener@gmx.de>
2017-06-30 02:05:52 +0200

    site.mk -v025 - gluon 2017.1

--- site.mk
+++ site.mk
@@ -151,7 +151,7 @@ endif
 #                       opkg compare-versions "$1" '>>' "$2"
 #               to decide if a version is newer or not.
 
-DEFAULT_GLUON_RELEASE := ffwss-v024
+DEFAULT_GLUON_RELEASE := ffwss-v025
 
 #       GLUON_RELEASE
 #               call make with custom GLUON_RELEASE flag, to use your own release version scheme.
