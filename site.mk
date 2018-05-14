e0916ba9544f6d028676da91477fd40b97d5bf9d
Tom <tsiener@gmx.de>
2018-05-14 19:37:38 +0200

    site.mk - v032 - gluon2017.1.x
    Gluon 2017.1.7 branch vom 13.05.2018commitID:
    12340afd9adf37d1823a5c79827e2af37e853686

--- site.mk
+++ site.mk
@@ -212,7 +212,7 @@ endif
 #                       opkg compare-versions "$1" '>>' "$2"
 #               to decide if a version is newer or not.
 
-DEFAULT_GLUON_RELEASE := ffwss-v031
+DEFAULT_GLUON_RELEASE := ffwss-v032
 
 #       GLUON_RELEASE
 #               call make with custom GLUON_RELEASE flag, to use your own release version scheme.
