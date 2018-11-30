66fface75fb61495f36acb9e9eef53c5b811026b
Tom <tsiener@gmx.de>
2018-11-30 11:00:33 +0100

    site.mk - gluon-ebtables-source-filter added

--- site.mk
+++ site.mk
@@ -11,6 +11,7 @@ GLUON_FEATURES := \
 	ebtables-filter-multicast \
 	ebtables-filter-ra-dhcp \
 	ebtables-limit-arp \
+	gluon-ebtables-source-filter \
 	mesh-batman-adv-15 \
 	mesh-vpn-tunneldigger \
 	radvd \
