2a7159b3aee453059de1c788fbdedc0e96557dca
Tom <tsiener@gmx.de>
2019-01-27 15:16:11 +0100

    site.mk - v036ms1

--- site.mk
+++ site.mk
@@ -221,7 +221,7 @@ endif
 #                       opkg compare-versions "$1" '>>' "$2"
 #               to decide if a version is newer or not.
 
-DEFAULT_GLUON_RELEASE := ffwss-v036ms
+DEFAULT_GLUON_RELEASE := ffwss-v036ms1
 
 
 #       GLUON_RELEASE
