f2f8ce72a7401c5b86f1f01fffdcbb3fa16ec913
Tom <tsiener@gmx.de>
2019-01-28 16:50:58 +0100

    site.mk - v036ms7 hostname debug

--- site.mk
+++ site.mk
@@ -223,7 +223,7 @@ endif
 #                       opkg compare-versions "$1" '>>' "$2"
 #               to decide if a version is newer or not.
 
-DEFAULT_GLUON_RELEASE := ffwss-v036ms6
+DEFAULT_GLUON_RELEASE := ffwss-v036ms7
 
 
 #       GLUON_RELEASE
