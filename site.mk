0bf02bbf20210373207416bb393d68aae18a6adc
Tom <tsiener@gmx.de>
2019-01-28 03:04:18 +0100

    site.mk - v036ms5
    commit im hostname package wieder rein

--- site.mk
+++ site.mk
@@ -223,7 +223,7 @@ endif
 #                       opkg compare-versions "$1" '>>' "$2"
 #               to decide if a version is newer or not.
 
-DEFAULT_GLUON_RELEASE := ffwss-v036ms4
+DEFAULT_GLUON_RELEASE := ffwss-v036ms5
 
 
 #       GLUON_RELEASE
