d1036e3ab71aafee1d832d1d91c444e4922d7889
Tom <tsiener@gmx.de>
2019-01-28 02:36:41 +0100

    site.mk - v036ms4
    default prefix leer

--- site.mk
+++ site.mk
@@ -223,7 +223,7 @@ endif
 #                       opkg compare-versions "$1" '>>' "$2"
 #               to decide if a version is newer or not.
 
-DEFAULT_GLUON_RELEASE := ffwss-v036ms3
+DEFAULT_GLUON_RELEASE := ffwss-v036ms4
 
 
 #       GLUON_RELEASE
