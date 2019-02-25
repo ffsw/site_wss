c4985bdc9f2f540a6ed9468b52284a64b4825552
Tom <tsiener@gmx.de>
2019-02-25 11:31:52 +0100

    site.mk - v033s - CPE
    cpe zwischenversion

--- site.mk
+++ site.mk
@@ -212,7 +212,7 @@ endif
 #                       opkg compare-versions "$1" '>>' "$2"
 #               to decide if a version is newer or not.
 
-DEFAULT_GLUON_RELEASE := ffwss-v032
+DEFAULT_GLUON_RELEASE := ffwss-v033s
 
 #       GLUON_RELEASE
 #               call make with custom GLUON_RELEASE flag, to use your own release version scheme.
