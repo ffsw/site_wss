d1f7486453d17bf8ec19c69340914ef59a83d1df
Tom <tsiener@gmx.de>
2020-01-26 23:50:38 +0100

    typo

--- site.mk
+++ site.mk
@@ -11,7 +11,7 @@ GLUON_FEATURES := \
 	ebtables-filter-ra-dhcp \
 	ebtables-source-filter \
 	mesh-batman-adv-15 \
-	mesh-vpn-tunneldigger \	
+	mesh-vpn-tunneldigger \
 	respondd \
 	status-page \
 	web-advanced \
