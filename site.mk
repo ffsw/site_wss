54ad5a95c4f2d626532e2458686e03f04dc819b8
Tom <tsiener@gmx.de>
2020-05-02 21:19:32 +0200

    Revert "site.mk v042 - gluon 2020.1.2"
    This reverts commit 050246d86a709b5feafd1ca475c6ab8edb503266.

--- site.mk
+++ site.mk
@@ -16,7 +16,6 @@ GLUON_FEATURES := \
 	status-page \
 	web-advanced \
 	web-wizard \
-	gluon-web-private-wifi \
 
 # radv-filterd\ erstmal weglassen - nicht sicher obs zusammen mit ebtables-filter-ra-dhcp funktioniert
 
@@ -193,7 +192,7 @@ endif
 #                       opkg compare-versions "$1" '>>' "$2"
 #               to decide if a version is newer or not.
 
-DEFAULT_GLUON_RELEASE := ffwss-v043
+DEFAULT_GLUON_RELEASE := ffwss-v042
 
 
 #       GLUON_RELEASE
