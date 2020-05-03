974436ecadb366f4ad6cc7b57eadcc7f566e693b
Tom <tsiener@gmx.de>
2020-05-04 01:48:23 +0200

    v043t6 - hostname tests

--- site.mk
+++ site.mk
@@ -195,7 +195,7 @@ endif
 #                       opkg compare-versions "$1" '>>' "$2"
 #               to decide if a version is newer or not.
 
-DEFAULT_GLUON_RELEASE := ffwss-v043t1
+DEFAULT_GLUON_RELEASE := ffwss-v043t6
 
 
 #       GLUON_RELEASE
