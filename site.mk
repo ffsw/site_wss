c30a1e6fb22080bab01e9cd7d40d36888a8960dd
Tom <tsiener@gmx.de>
2024-10-07 09:56:55 +0200

    V053 für gluon 2023.2.4

--- site.mk
+++ site.mk
@@ -150,7 +150,7 @@ endif
 #                       opkg compare-versions "$1" '>>' "$2"
 #               to decide if a version is newer or not.
 
-DEFAULT_GLUON_RELEASE := ffwss-v052
+DEFAULT_GLUON_RELEASE := ffwss-v053
 
 
 #       GLUON_RELEASE
